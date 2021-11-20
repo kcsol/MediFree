61c3d1f71ec2bb83af303e86747a9db1765ad9bf
kcsol <62585142+kcsol@users.noreply.github.com>
2021-11-20 22:15:24 +0900

    rename project

--- app/src/main/java/com/knu/medifree/MainActivity.kt
+++ app/src/main/java/com/knu/medifree/MainActivity.kt
@@ -3,6 +3,7 @@ package com.knu.medifree
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import com.knu.medifree.functions.Account
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/com/knu/medifree/functions/Account.kt
+++ app/src/main/java/com/knu/medifree/functions/Account.kt
@@ -1,4 +1,4 @@
-package com.knu.medifree
+package com.knu.medifree.functions
 
 import android.content.Context
 import android.util.Log
