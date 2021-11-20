dbaa49e3ef2f664edcdf8acb41d01032c4ab63b1
kcsol <62585142+kcsol@users.noreply.github.com>
2021-11-20 22:07:25 +0900

    start

---
+++ app/src/main/java/com/knu/medifree/Account.kt
@@ -0,0 +1,114 @@
+package com.knu.medifree
+
+import android.content.Context
+import android.util.Log
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+
+/*
+ * 변수명 = Account(this.applicationContext)
+ * Context는 Toast를 띄우기 위해 전달됩니다.
+ */
+class Account(val context: Context) {
+    companion object {
+        val TYPE_PATIENT = 0
+        val TYPE_DOCTOR = 1
+    }
+
+
+    val mAuth = FirebaseAuth.getInstance()
+    val db = FirebaseFirestore.getInstance()
+
+    /*
+     * account.signUp(emailString, passwordString)
+     * firebase상에 중복된 email이 존재하면 null을 return합니다.
+     * email 혹은 password가 양식이 잘못되면 null을 return합니다.
+     */
+    fun signUp(email:String, password:String): String? {
+
+        var uid:String? = null
+        // Firebase 계정 생성
+        mAuth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener {
+                    task->
+                if(task.isSuccessful) {
+                    Toast.makeText(context, "회원가입에 성공했습니다.", Toast.LENGTH_SHORT).show()
+                    Log.i("register", "성공")
+                    uid = mAuth.currentUser!!.uid
+                }
+                else {
+                    Toast.makeText(context, "회원가입에 실패했습니다.", Toast.LENGTH_SHORT).show()
+                    Log.i("register", "실패")
+                }
+            }
+
+        /*
+        // DB 저장
+        var user = mutableMapOf<String, Any>()
+        user.put("userType", userType)
+        user.put("name", name)
+        user.put("phone", phone)
+
+        db.collection("Profile").document(uid).set(user)
+            .addOnSuccessListener {
+                tmp = 0
+            }
+            .addOnFailureListener {
+                tmp = 1
+            }
+
+         */
+
+        return uid
+    }
+
+    /*
+     * account.signIn(emailString, passwordString)
+     * 로그인이 성공하면 Pair(uid, userType)을 반환합니다.
+     * 현재는 DB가 구현되지 않아 userType은 항상 null입니다.
+     * 로그인이 실패하면 Pair(null, null)을 반환합니다.
+     */
+    fun signIn(email:String, password:String): Pair<String?, String?> {
+
+        if(email.length <= 0 || password.length <= 0) {
+            return Pair(null, null)
+        }
+
+        var uid: String? = null
+        var userType: String? = null
+
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener {
+                task ->
+            if(task.isSuccessful) {
+                var user = mAuth.currentUser
+                uid = user!!.uid
+                Toast.makeText(context, "로그인에 성공했습니다.", Toast.LENGTH_SHORT).show()
+                Log.i("login", "성공")
+                /*
+                val docRef = db.collection("Profile").document(uid)
+                docRef.get().addOnCompleteListener {
+                        task->
+                    if(task.isSuccessful) {
+                        val doc = task.result!!
+                        if (doc.exists()) {
+                            val userType = doc.data!!.get("userType").toString()
+                            if (userType == "Patient") {
+                                //DBManager.initDBManager(uid, TYPE_PATIENT)
+                            } else if (userType == "Doctor") {
+                                //DBManager.initDBManager(uid, TYPE_DOCTOR)
+                            }
+                        }
+                    }
+                }
+                 */
+            } else {
+                Toast.makeText(context, "로그인에 실패했습니다.", Toast.LENGTH_SHORT).show()
+                Log.i("login", "실패")
+            }
+        }
+
+        return Pair(uid, userType)
+    }
+
+}
---
+++ app/src/main/java/com/knu/medifree/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.knu.medifree
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val account = Account(this.applicationContext)
+        var uid = account.signUp("test@abc.com", "password123!@#")
+        Log.i("uid", uid.toString())
+
+        var signIn = account.signIn("test@abc.com", "password123!@#")
+        Log.i("signIn", signIn.toString())
+    }
+}
