b18f76eac82d1b3c8195f29f9419958fb5fd5184
kcsol <62585142+kcsol@users.noreply.github.com>
2021-11-27 14:20:08 +0900

    Doctor, Patient to static

--- app/src/main/java/com/knu/medifree/functions/Doctor.kt
+++ app/src/main/java/com/knu/medifree/functions/Doctor.kt
@@ -5,7 +5,6 @@ import android.util.Log
 class Doctor {
 
     companion object {
-<<<<<<< HEAD
         lateinit var doctor:Map<String, Any>
         fun setDoctor(uid:String) {
             doctor = DBManager.load(DBManager.PROFILE, uid)!!
@@ -39,56 +38,12 @@ class Doctor {
             DBManager.update(DBManager.RESERVATION, reservNum, "is_completed", 1)
 
             val toWrite = searchDoctorToWrite() as MutableList<String>
-=======
-        lateinit var uid:String
-        lateinit var doctor: Map<String, Any>
-        fun setDoctor(uid:String) {
-            this.uid = uid
-            doctor = DBManager.load(DBManager.PROFILE, uid)!!
-        }
-
-        fun searchToWrite(): List<String>? {
-            val toWrite = DBManager.load(
-                DBManager.DOCTOR,
-                doctor["name"].toString()
-            )!!["작성할것"] as List<String>
-
-            return toWrite
-        }
-
-        fun searchToConfirm(): List<String>? {
-            val toConfirm = DBManager.load(
-                DBManager.DOCTOR,
-                doctor["name"].toString()
-            )!!["확인할것"] as List<String>
-
-            return toConfirm
-        }
-
-        fun searchSchedule(date: String): List<String>? {
-            val schedule =
-                DBManager.load(DBManager.DOCTOR, doctor["name"].toString())!![date] as List<String>
-
-            return schedule
-        }
-
-        fun searchReservationInfo(reservNum: String): Map<String, Any?>? {
-            return DBManager.load(DBManager.RESERVATION, reservNum)
-        }
-
-        fun addQuestion(reservNum: String, doctorName: String, questions: List<String>) {
-            DBManager.update(DBManager.RESERVATION, reservNum, "질문", questions)
-            DBManager.update(DBManager.RESERVATION, reservNum, "is_completed", 1)
-
-            val toWrite = searchToWrite() as MutableList<String>
->>>>>>> e04dde0b399f323eb7cea281272aadafa697158c
             toWrite.remove(reservNum)
 
             DBManager.update(DBManager.DOCTOR, doctorName, "작성할것", toWrite)
         }
-<<<<<<< HEAD
 
-        fun addDiagnosisType(reservNum:String, diagnosisType:Int) {
+        fun addDiagnosisType(reservNum: String, diagnosisType: Int) {
             DBManager.update(DBManager.RESERVATION, reservNum, "diagnosis_type", diagnosisType)
 
             val toConfirm = searchDoctorToConfirm() as MutableList<String>
@@ -96,18 +51,6 @@ class Doctor {
 
             DBManager.update(DBManager.DOCTOR, doctor["name"] as String, "확인할것", toConfirm)
         }
-
-=======
-
-        fun addDiagnosisType(reservNum: String, diagnosisType: Int) {
-            DBManager.update(DBManager.RESERVATION, reservNum, "diagnosis_type", diagnosisType)
-
-            val toConfirm = searchToConfirm() as MutableList<String>
-            toConfirm.remove(reservNum)
-
-            DBManager.update(DBManager.DOCTOR, doctor["name"] as String, "확인할것", toConfirm)
-        }
->>>>>>> e04dde0b399f323eb7cea281272aadafa697158c
     }
 
 }
--- app/src/main/java/com/knu/medifree/functions/Patient.kt
+++ app/src/main/java/com/knu/medifree/functions/Patient.kt
@@ -9,10 +9,7 @@ class Patient {
         val MAJOR_0 = 0
         val MAJOR_1 = 1
         // 필요한 MAJOR 추가, naming
-<<<<<<< HEAD
-=======
 
->>>>>>> e04dde0b399f323eb7cea281272aadafa697158c
 
         lateinit var uid:String
         lateinit var patient:Map<String, Any>
@@ -34,7 +31,6 @@ class Patient {
                     return null
                 }
             }
-<<<<<<< HEAD
 
             return major
         }
@@ -64,80 +60,6 @@ class Patient {
                     selectable[i] = true
             }
 
-=======
-
-            return major
-        }
-
-        fun searchHospital(majorType:Int): List<String>? {
-            val major = convertMajor(majorType)
-            val hospitals = DBManager.load(DBManager.MAJOR, major!!)!!["병원명"] as List<String>
-
-            return hospitals
-        }
-
-        fun searchDoctorInHospital(hospital:String, majorType:Int): List<String>? {
-            val major = convertMajor(majorType)
-            val doctors = DBManager.load(DBManager.HOSPITAL, hospital)!![major] as List<String>
-
-            return doctors
-        }
-
-        fun searchDoctorSchedule(doctor:String, date:String): List<Boolean>? {
-            val schedules = DBManager.load(DBManager.DOCTOR, doctor)
-
-            // 몇 개로 나눌건지에 따라 list 길이랑 for문 반복수 늘리기
-            var selectable = mutableListOf(false, false, false, false)
-            val schedule = schedules!![date] as List<String>
-            for (i in 0..3) {
-                if(schedule[i] == null)
-                    selectable[i] = true
-            }
->>>>>>> e04dde0b399f323eb7cea281272aadafa697158c
-
-            return selectable
-        }
-
-<<<<<<< HEAD
-        fun searchPatientReservationList(): List<String>? {
-            return patient!!["예약번호"] as List<String>
-        }
-
-        fun addNewReservation(doctorName:String, date:String, time:Int) {
-            val reservNum = "testabcdefgsomethingcomplexstringarray22"
-
-            val reservation = hashMapOf(
-                "diagnosis_date" to date,
-                "diagnosis_time" to time,
-                "diagnosis_type" to 0,
-                "doctor_name" to doctorName,
-                "patient_name" to patient!!["name"],
-                "is_completed" to 0,
-                "room_open" to 0,
-                "질문" to listOf("", "", "", ""),
-                "답변" to listOf("", "", "", "")
-            )
-            DBManager.save(DBManager.RESERVATION, reservNum, reservation)
-
-            val doctor = DBManager.load(DBManager.DOCTOR, doctorName)
-            val dReservList = doctor!![date] as MutableList<String>
-            dReservList[time] = reservNum
-            DBManager.update(DBManager.DOCTOR, doctorName, date, dReservList)
-            DBManager.add(DBManager.DOCTOR, doctorName, "작성할것", reservNum)
-            DBManager.add(DBManager.PROFILE, uid, "예약번호", reservNum)
-        }
-
-        fun searchReservationInfo(reservNum:String): Map<String, Any?>? {
-            return DBManager.load(DBManager.RESERVATION, reservNum)
-        }
-
-        fun addAnswer(reservNum:String, doctorName:String, answers:List<String>) {
-            DBManager.update(DBManager.RESERVATION, reservNum, "답변", answers)
-            DBManager.update(DBManager.RESERVATION, reservNum, "is_completed", 2)
-            DBManager.add(DBManager.DOCTOR, doctorName, "확인할것", reservNum)
-        }
-
-=======
             return selectable
         }
 
@@ -178,7 +100,6 @@ class Patient {
             DBManager.update(DBManager.RESERVATION, reservNum, "is_completed", 2)
             DBManager.add(DBManager.DOCTOR, doctorName, "확인할것", reservNum)
         }
->>>>>>> e04dde0b399f323eb7cea281272aadafa697158c
     }
 
 }
